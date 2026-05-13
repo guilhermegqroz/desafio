35dbbb9d5685e708dc443f6914c4a8d50a8ae837
Guilherme Queiroz <ggeraldodequeiroz@gmail.com>
2026-05-13 19:23:41 -0300

    feat: implementa tratamento global de exceções

---
+++ projectportfolioapi/src/main/java/com/guilherme/projectportfolioapi/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.guilherme.projectportfolioapi.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+}
---
+++ projectportfolioapi/src/main/java/com/guilherme/projectportfolioapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.guilherme.projectportfolioapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+            ResourceNotFoundException ex
+    ) {
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+}
