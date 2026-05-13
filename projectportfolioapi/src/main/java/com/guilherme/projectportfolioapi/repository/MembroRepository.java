510d9374f0bccc945928803ab868747dcb9f4ce4
Guilherme Queiroz <ggeraldodequeiroz@gmail.com>
2026-05-13 16:58:08 -0300

    "feat: adiciona repositories de projeto e membro"

---
+++ projectportfolioapi/src/main/java/com/guilherme/projectportfolioapi/repository/MembroRepository.java
@@ -0,0 +1,9 @@
+package com.guilherme.projectportfolioapi.repository;
+
+import com.guilherme.projectportfolioapi.entity.Membro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MembroRepository extends JpaRepository<Membro, UUID> {
+}
