b7c5b39f1c6c4582007c8d1e4f08647a0a2b7b0c
Guilherme Queiroz <ggeraldodequeiroz@gmail.com>
2026-05-13 15:57:36 -0300

    Criando projeto para realizar o desafio.

---
+++ projectportfolioapi/src/main/java/com/guilherme/projectportfolioapi/ProjectportfolioapiApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.projectportfolioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectportfolioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectportfolioapiApplication.class, args);
+	}
+
+}
---
+++ projectportfolioapi/src/test/java/com/guilherme/projectportfolioapi/ProjectportfolioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.projectportfolioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectportfolioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
